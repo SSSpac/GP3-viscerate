dd7f59213e1113c8cd3e5e4001d7a3aa48d9dc93
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-03 18:24:36 +0200

    Responsiveness small fixes

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,7 +4,7 @@ import FrameWrapper from "../../components/UI/FrameWrapper";
 const GamePage = () => {
   return (
     <div className="w-full h-full relative font-inter">
-      <div className="md:w-[80%] h-[80vh] w-full">
+      <div className="md:w-[80%] h-[70vh] md:h-[80vh] w-full">
         <FrameWrapper>
           <img
             src="/images-game-desc/mock-image1.png"
@@ -12,16 +12,19 @@ const GamePage = () => {
             className="w-full h-full object-cover"
           />
         </FrameWrapper>
-        <div className="max-w-xl absolute bottom-4 w-full text-center md:top-1/2 md:right-30 md:bottom-auto md:text-right md:transform md:-translate-y-4/5 space-y-8 text-base">
-          <h1 className="md:text-2xl lg:text-5xl p-2">
-            “Amazing descriptive text placeholder”
-          </h1>
-          <h4 className="text-sm md:text-xl lg:text-2xl">
-            “even more text placeholder”
-          </h4>
-        </div>
       </div>
-      <ScrollDownButton text={`"Scroll for more info"`} navigateTo="content" />
+      <div className="max-w-xl absolute w-full bottom-10 text-center md:top-1/2 md:right-10  lg:right-30 md:bottom-auto md:text-right md:transform md:-translate-y-4/5 md:space-y-8 text-base">
+        <h1 className="md:text-3xl lg:text-5xl p-2">
+          “Amazing descriptive text placeholder”
+        </h1>
+        <h4 className="text-sm md:text-xl lg:text-2xl">
+          “even more text placeholder”
+        </h4>
+      </div>
+
+      <div className="absolute -bottom-20 md:-bottom-18 left-1/2 transform -translate-x-1/2 z-10">
+        <ScrollDownButton text="Scroll for more info" navigateTo="content" />
+      </div>
     </div>
   );
 };
