b2ac7d68fc5fb8829e2ed6c1a4b0805fafe3709b
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-03 18:13:43 +0200

    Do responsiveness for Desktop and mobile

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,16 +3,26 @@ import FrameWrapper from "../../components/UI/FrameWrapper";
 
 const GamePage = () => {
   return (
-    <section className="w-[100%] h-[75dvh]">
-      <FrameWrapper>
-        <img
-          src="/images-game-desc/mock-image1.png"
-          alt="mock image"
-          className="w-full h-full object-cover"
-        />
-      </FrameWrapper>
-      <ScrollDownButton text="Scroll Down" navigateTo="content" />
-    </section>
+    <div className="w-full h-full relative font-inter">
+      <div className="md:w-[80%] h-[80vh] w-full">
+        <FrameWrapper>
+          <img
+            src="/images-game-desc/mock-image1.png"
+            alt="mock image"
+            className="w-full h-full object-cover"
+          />
+        </FrameWrapper>
+        <div className="max-w-xl absolute bottom-4 w-full text-center md:top-1/2 md:right-30 md:bottom-auto md:text-right md:transform md:-translate-y-4/5 space-y-8 text-base">
+          <h1 className="md:text-2xl lg:text-5xl p-2">
+            “Amazing descriptive text placeholder”
+          </h1>
+          <h4 className="text-sm md:text-xl lg:text-2xl">
+            “even more text placeholder”
+          </h4>
+        </div>
+      </div>
+      <ScrollDownButton text={`"Scroll for more info"`} navigateTo="content" />
+    </div>
   );
 };
 
--- src/components/ScrollDownButton.tsx
+++ src/components/ScrollDownButton.tsx
@@ -5,7 +5,7 @@ const ScrollDownButton = ({ text, navigateTo }: ScrollDownButtonType) => {
   return (
     <Link
       href={`#${navigateTo}`}
-      className="flex flex-col justify-center items-center animate-bounce text-sm gap-2"
+      className="flex flex-col justify-center items-center animate-bounce text-sm gap-2 z-10 font-inter"
     >
       {text}
       <img src="/images-game-desc/triangle-icon.png" alt="triangle icon" width={32} />
