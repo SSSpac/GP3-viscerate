e235a0829af6257abaa2b3b357f18130bc035ef0
SSSpac <saifeisa820@gmail.com>
2025-06-04 17:29:46 +0200

    changed header style from module css to tailwind
    deleted place holder pictures, and using tailwind for styling

--- src/components/hp-header/index.tsx
+++ src/components/hp-header/index.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
-import styles from './header.module.css';
 
 const HpHeader = () => {
     return (
-        <header className={styles.header}>
-             <Link href="/developers" className={styles.link}>ABOUT DEVELOPERS</Link>
+        <header className="fixed top-0 w-full bg-[#000D15] text-white py-5 font-thin opacity-100 tracking-wide z-50 min-h-[80px] flex items-center md:justify-end justify-center">
+             <Link 
+                href="/developer" 
+                className="md:relative md:right-28 font-['Aboreto'] text-xl hover:text-gray-300 transition-colors md:mr-7 tracking-wider"
+             >
+                ABOUT DEVELOPERS
+             </Link>
         </header>
     )
 }
+
 export default HpHeader;
