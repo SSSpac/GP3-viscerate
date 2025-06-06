7445360b1196b4593ce2af9a69bb10e2007951ea
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-06-06 21:06:40 +0200

    made review changes

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 import { GameMechanicCardProps } from "@/types/globals";
 
-const GameCard: React.FC<GameMechanicCardProps> = ({ title, imageSrc, alt }) => {
+const GameCard = ({ title, imageSrc, alt }:GameMechanicCardProps) => {
   return (
     <div className="p-4 w-full flex justify-center">
       <div className="w-full max-w-[513px]">
        
-        <div className="relative w-full h-[180px] md:h-[288px] md:w-[513px] mx-auto">
+        <div className="relative w-full h-[180px] sm:h-[220px] md:h-[288px]">
           <Image
             src={imageSrc}
             alt={alt || title || "Game mechanic example"}
             fill
             priority
             className="object-cover rounded-md"
-            sizes="(min-width: 768px) 513px, 100vw"
+            sizes="(min-width: 1024px) 513px, (min-width: 768px) 90vw,100vw"
           />
         </div>
-        <p className="mt-6 text-[var(--extraground)] font-inter text-center w-full md:text-[38px] sm:text-[24px] md:w-[513px] mx-auto">
+        <p className="mt-6 text-[var(--extraground)] font-inter text-center text-sm sm:text-lg md:text-2xl lg:text-[38px] whitespace-nowrap w-full mx-auto truncate">
           “{title}”
         </p>
       </div>
