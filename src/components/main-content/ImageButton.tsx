23d30c8a82668793c7936e57787c02a3f9df9f21
SSSpac <saifeisa820@gmail.com>
2025-06-08 11:36:57 +0200

    .

--- src/components/main-content/ImageButton.tsx
+++ src/components/main-content/ImageButton.tsx
@@ -1,41 +1,28 @@
-import Image from 'next/image';
+import Image from 'next/image'
+import Link from 'next/link'
 
+interface ImageButtonProps {
+  src: string;
+  alt: string;
+  href: string;
+  width: number;
+  height: number;
+  onClick?: () => void;
+}
 
-const ImageButton = ({ 
-  src, 
-  alt, 
-  href, 
-  onClick, 
-  width = 850, 
-  height = 60 
-}) => {
-  const buttonClass = " hover:-translate-y-1 transition-all duration-300 ease-in-out cursor-pointer";
-  const imageClass = "w-[500px] h-[60px] object-contain hover:opacity-60";
-  
-  const content = (
-    <Image
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-      className={imageClass}
-    />
-  );
+const ImageButton = ({ src, alt, href, width, height, onClick }: ImageButtonProps) => {
+  return (
+    <Link href={href}>
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        onClick={onClick}
+        className="hover:opacity-80 transition-opacity cursor-pointer"
+      />
+    </Link>
+  )
+}
 
-  return href ? (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={buttonClass}
-    >
-      {content}
-    </a>
-  ) : (
-    <button onClick={onClick} className={buttonClass}>
-      {content}
-    </button>
-  );
-};
-
-export default ImageButton;
+export default ImageButton
