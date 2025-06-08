6f124d3cd2384d51e02a40162b4d65b399909f24
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-08 14:51:11 +0200

    moved data folder

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { teamMembers } from '../../../public/data/teamMembers';
+import { teamMembers } from '../../data/teamMembers';
 import { TeamMember } from '@/types/globals';
 
 
