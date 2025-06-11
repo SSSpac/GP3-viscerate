d1131ecf825c9c7ba651dbec82d5e2adbf43f19b
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-11 23:46:35 +0200

    Add team-data

---
+++ src/data/team-data.ts
@@ -0,0 +1,43 @@
+export const teamMembers = [
+  {
+    category: "Game Design",
+    members: [
+      { name: "Zonghao Wu", role: "PO, Lvl - Gameplay" },
+      { name: "Jacob Yosef", role: "System Generalist" },
+      { name: "Max Hjort Tech", role: "Narr/Sound" },
+    ],
+  },
+  {
+    category: "Game Artists",
+    members: [
+      { name: "Max Strömdal Erikssojn", role: "Character Artist" },
+      { name: "Xiyuan Wang", role: "Environment - Artist" },
+      { name: "Ira Aspelin", role: "Character prop specifically" },
+    ],
+  },
+  {
+    category: "Game Programmers",
+    members: [
+      { name: "Kiran Gaurdian George", role: "Game Programmer" },
+      { name: "Mohammed Diab", role: "Game Programmer" },
+      { name: "Tiger Martin", role: "Game Programmer" },
+      { name: "Sebastian Valck", role: "Game Programmer" },
+    ],
+  },
+  {
+    category: "ANIM&VFX",
+    members: [
+      { name: "Jesper Turesson", role: "VFX Artist" },
+      { name: "Sushil Jangra", role: "Animator" },
+    ],
+  },
+  {
+    category: "Management",
+    members: [
+      { name: "Alvaro Ravello", role: "Game Producer" },
+      { name: "Ammara Waqas", role: "Project Manager" },
+      { name: "Ola Jansson", role: "Release Manager" },
+      { name: "Mayc Lövgren", role: "Release Manager" },
+    ],
+  },
+];
