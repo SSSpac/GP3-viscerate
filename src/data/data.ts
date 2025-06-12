d5681dc54833388b7815f9b1f9d8377434053cd1
bushra-rauf <bush_rauf@yahoo.com>
2025-06-12 03:03:06 +0200

    moved data.ts in to data folder

--- src/data/data.ts
+++ src/data/data.ts
@@ -12,3 +12,11 @@ export type features = [
     imageSrc: "/images/GM-example3.jpg",
   },
 ];
+
+import { LinkItem } from "../types/globals";
+
+export const link:LinkItem = {
+  href: "/", 
+  text: "Back" 
+
+};
