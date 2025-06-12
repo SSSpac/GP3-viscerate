58f0e0aaaae777a0d39d614d015cb0a000d3c955
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-12 14:24:52 +0200

    Move id="content" to inner div

--- src/components/DescriptionSection/index.tsx
+++ src/components/DescriptionSection/index.tsx
@@ -4,8 +4,8 @@ import { DescriptionSectionProps } from "@/types/globals";
 const DescriptionSection = ({id, imageAlt, text, index}: DescriptionSectionProps & {index: number}) => {
     const isImageRight = index% 2 === 0 ;
     return (
-        <section id="content" className={`flex flex-row items-center gap-6   ${isImageRight ?  "" :  "flex-row-reverse"} `}>
-            <div className="flex justify-center mt-[70px] mb-[30px] w-full h-[200px] bg-cover bg-center
+        <section className={`flex flex-row items-center gap-6   ${isImageRight ?  "" :  "flex-row-reverse"} `}>
+            <div id="content" className="flex justify-center mt-[70px] mb-[30px] w-full h-[200px] bg-cover bg-center
             sm:w-[80%] sm:h-auto sm:my-[90px] sm:min-h-[300px] bg-[url(/descSec.png)]" aria-label={imageAlt}>
                 <div className={`text-sm text-extraground p-5 text-center bottom-12 relative 
                     sm:text-2xl sm:bottom-0 sm:text-left sm:p-0 sm:w-60 sm:flex flex-col justify-center ${isImageRight ?  "sm:left-[47%]" :  "sm:right-[45%]"} `}>
