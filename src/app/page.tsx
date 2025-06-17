c61887a4ea830ffaaa6fbd9965aaed1d1660de8f
SSSpac <saifeisa820@gmail.com>
2025-06-17 19:58:38 +0200

    main fixes to branches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,32 @@ import GameCard from '@/components/GameCard';
 import Hpheader from '@/components/hp-header';
 import MainContent from '@/components/main-content/MainContent';
 import PageTransition from '@/components/Transition';
+import Image from 'next/image';
 
 
 export default function Home() {
   return (
-             <PageTransition>
-
+    <PageTransition>
    <div>
       <Hpheader />      
       <MainContent />
-
+      <h1 className="text-[#858585] mt-8 font-bold text-center text-2xl"> Discover our game </h1>
       <main className="bg-[var(--background)] min-h-screen flex flex-col items-center justify-center py-10 min-h-screen w-full overflow-x-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10">
           <GameCard
-            imageSrc="/images/GM-example1.jpg"
-            title="Game mechanic example1"
+            imageSrc="/images-game-desc/Dark.png"
+            title="Goth Castle"
           />
           <GameCard
-            imageSrc="/images/GM-example2.jpg"
-            title="Game mechanic example2"
+            imageSrc="/images-game-desc/Grey.png"
+            title="Toxic Zone"
           />
         </div>
 
-        <div className="mt-6">
+        <div className="w-full mt-6 mb-6">
           <GameCard
-            imageSrc="/images/GM-example3.jpg"
-            title="Game mechanic example3"
+            imageSrc="/images-game-desc/Atomic.png"
+            title="Bio-Punk Horror"
           />
         </div>
       </main>
