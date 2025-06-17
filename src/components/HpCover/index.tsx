c004c9315be5a63f01246569a7ac316db51f90a6
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-17 20:03:48 +0200

    changes

--- src/components/HpCover/index.tsx
+++ src/components/HpCover/index.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import React from 'react'
 import { motion } from 'motion/react'
 
 export default function HpCover() {
   return (
-    <div className='flex justify-center items-centern absolute top-0 bottom-0 w-full bg-foreground'>
+    <div className='fixed flex justify-center items-center top-0 bottom-0 w-full bg-foreground z-70'>
+        <motion.button className='w-100 h-100'>
+            <img className='w-full h-full'  src="./icons/logo_2.png" alt="" />
+        </motion.button>
     </div>
   )
 }
