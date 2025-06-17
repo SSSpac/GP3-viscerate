ae5114e0ecd9a137a04e1377a74009d14db4cdad
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-17 18:56:33 +0200

    added motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,43 @@
 import GameCard from '@/components/GameCard';
 import Hpheader from '@/components/hp-header';
+import HpCover from '@/components/HpCover';
 import MainContent from '@/components/main-content/MainContent';
 import PageTransition from '@/components/Transition';
 
 
 export default function Home() {
   return (
-             <PageTransition>
+    <>
+      <HpCover />
+      <PageTransition>
+        <div>
+          <Hpheader />
+          <MainContent />
 
-   <div>
-      <Hpheader />      
-      <MainContent />
+          <main className="bg-[var(--background)] flex flex-col items-center justify-center py-10 min-h-screen w-full overflow-x-hidden">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+              <GameCard
+                imageSrc="/images/GM-example1.jpg"
+                title="Game mechanic example1"
+              />
+              <GameCard
+                imageSrc="/images/GM-example2.jpg"
+                title="Game mechanic example2"
+              />
+            </div>
 
-      <main className="bg-[var(--background)] min-h-screen flex flex-col items-center justify-center py-10 min-h-screen w-full overflow-x-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-          <GameCard
-            imageSrc="/images/GM-example1.jpg"
-            title="Game mechanic example1"
-          />
-          <GameCard
-            imageSrc="/images/GM-example2.jpg"
-            title="Game mechanic example2"
-          />
+            <div className="mt-6">
+              <GameCard
+                imageSrc="/images/GM-example3.jpg"
+                title="Game mechanic example3"
+              />
+            </div>
+          </main>
         </div>
+      </PageTransition>
+    </>
 
-        <div className="mt-6">
-          <GameCard
-            imageSrc="/images/GM-example3.jpg"
-            title="Game mechanic example3"
-          />
-        </div>
-      </main>
-    </div>
-          </PageTransition>
-  
+
   );
 }
 
-      
---
+++ src/components/HpCover/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { motion } from 'motion/react'
+
+export default function HpCover() {
+  return (
+    <div className='flex justify-center items-centern absolute top-0 bottom-0 w-full bg-foreground'>
+    </div>
+  )
+}
