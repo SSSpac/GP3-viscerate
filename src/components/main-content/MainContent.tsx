8f5decffcc91c7eb950ece3fba774cf3f7dc3331
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-18 23:12:10 +0700

    Add h1 and make it hidden

--- src/components/main-content/MainContent.tsx
+++ src/components/main-content/MainContent.tsx
@@ -6,8 +6,8 @@ import ImageButton from "./ImageButton";
 const MainContent = () => {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-[url('/images/bg_3.png')] bg-cover bg-fixed bg-no-repeat bg-top min-h-screen px-6">
-
       <section className="flex justify-center items-center">
+        <h1 className="hidden">Viscerate</h1>
         <Image
           src="/images/title_2.png"
           alt="VISCERATE"
@@ -17,7 +17,6 @@ const MainContent = () => {
           className="flex justify-between w-full max-w-[1000px] h-auto drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-all duration-300 ease-in-out hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)] py-12 md:py-0 pt-24 md:pt-0"
         />
       </section>
-
       <div className="flex justify-center items-center gap-4 md:gap-6 w-auto max-w-[900px] flex-col md:flex-row">
         <ImageButton
           src="/icons/new.download-now.png"
@@ -26,7 +25,6 @@ const MainContent = () => {
           width={300}
           height={100}
         />
-
         <ImageButton
           src="/icons/new.learn-more.png"
           alt="Learn More"
@@ -35,7 +33,6 @@ const MainContent = () => {
           height={100}
         />
       </div>
-
       <section className="w-full max-w-[700px] text-center p-2 md:pt-6">
         <p className="text-hero-body text-md md:text-lg leading-relaxed tracking-tighter md:tracking-tight pt-4 md:pt-0">
           Viscerate is a fast-paced first-person shooter blending roguelite
