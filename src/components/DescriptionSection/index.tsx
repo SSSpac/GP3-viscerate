b449e102fba3acacb0756bf2e42b698c5732afe2
SSSpac <saifeisa820@gmail.com>
2025-06-18 20:14:07 +0200

    updated game description images

--- src/components/DescriptionSection/index.tsx
+++ src/components/DescriptionSection/index.tsx
@@ -1,24 +1,37 @@
 import { DescriptionSectionProps } from "@/types/globals";
+import Image from "next/image";
 
-
-const DescriptionSection = ({id, imageAlt, text, index}: DescriptionSectionProps & {index: number}) => {
-    const isImageRight = index% 2 === 0 ;
+const DescriptionSection = ({id, imageAlt, text, index, imageSrc}: DescriptionSectionProps & {index: number}) => {
+    const isImageRight = index % 2 === 0;
+    
     return (
-        <section className={`${index === 1 ? 'mt-30' : 'mt-0'} `}>
-            <div id="content" className={`flex flex-row items-center gap-6 w-full max-w-[100%] overflow-x-hidden px-4 md:px-6 lg:px-8  ${isImageRight ?  "" :  "flex-row-reverse"} `}>
-                <div className="flex justify-center mt-[70px] mb-[30px] w-full h-[200px] bg-cover bg-center
-                sm:w-[80%] sm:h-auto sm:my-[90px] sm:min-h-[300px] bg-[url(/descSec.png)]" aria-label={imageAlt}>
-                    <div className={`text-sm text-extraground p-5 text-center bottom-12 relative 
-                        sm:text-2xl sm:bottom-0 sm:text-left sm:p-0 sm:w-60 sm:flex flex-col justify-center ${isImageRight ?  "sm:left-[47%]" :  "sm:right-[45%]"} `}>
-                        {text.map((text, index) => (
-                            <p className="" key={index}>{text}</p>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-
+      <section className={`${index === 1 ? 'mt-30' : 'mt-0'}`}>
+  <div
+    id="content"
+    className={`flex flex-row items-center gap-6 w-full max-w-[100%] overflow-x-hidden px-4 md:px-6 lg:px-8 
+      ${isImageRight ? "" : "flex-row-reverse"}`}
+  >
+    <div className="relative flex justify-center mt-[70px] mb-[30px] w-full h-[200px] 
+                    sm:w-[80%] sm:h-auto sm:my-[90px] sm:min-h-[300px]">
+      <Image
+        src={imageSrc}
+        alt={imageAlt}
+        fill 
+        sizes=" (max-width: 1000px) 90vw, 1000px"
+        priority={index === 0}
+      />
+      <div
+        className={`text-sm text-extraground p-5 text-center bottom-12 relative 
+          sm:text-2xl sm:bottom-0 sm:text-left sm:p-0 sm:w-60 sm:flex flex-col justify-center 
+          ${isImageRight ? "sm:left-[47%]" : "sm:right-[45%]"}`}
+      >
+        {text.map((t, i) => (
+          <p key={i}>{t}</p>
+        ))}
+      </div>
+    </div>
+  </div>
+</section>
+    );
 }
-
-export default DescriptionSection
+export default DescriptionSection;
--- src/data/descriptionSection.ts
+++ src/data/descriptionSection.ts
@@ -1,29 +1,23 @@
 import { DescriptionSectionProps } from "@/types/globals";
 
 
-export const descriptionSectiondata: DescriptionSectionProps[] = [
-    {
-        id: 1,
-        imageAlt: "",
-        text: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        ]
-    },
-    {
-        id: 2,
-        imageAlt: "",
-        text: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        ]
-    },
-    {
-        id: 3,
-        imageAlt: "",
-        text: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        ]
-    }
-]
+    export const descriptionSectiondata: DescriptionSectionProps[] = [
+  {
+    imageSrc: "/images/game-menu.jpeg",
+    id: 1,
+    imageAlt: "Epic Battles",
+    text: ["Face epic battles", "And many boss encounters"]
+  },
+  {
+    imageSrc: "/images/gameplay_1.png",
+    id: 2,
+    imageAlt: "Explore World",
+    text: ["Explore a rich", "Land dynamic world"]
+  },
+  {
+    imageSrc: "/images/bg_4.png",
+    id: 3,
+    imageAlt: "Combat Gameplay",
+    text: ["Experience intense and", "Close and wide combat"]
+  }
+];
