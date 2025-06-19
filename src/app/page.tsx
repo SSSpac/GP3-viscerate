3c81d78e9377cea8d23949bb27133df4dc075f5c
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-19 21:29:35 +0700

    Some leftover merge conflicts on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@
 import GameCard from '@/components/GameCard';
 import Hpheader from '@/components/hp-header';
 import MainContent from '@/components/main-content/MainContent';
-import Image from 'next/image';
 import PageTransition from '@/components/Transition';
-import Image from 'next/image';
-
 
 export default function Home() {
   return (
@@ -19,33 +16,19 @@ export default function Home() {
         <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10">
 
           <GameCard
-<<<<<<< HEAD
-            imageSrc="/icons/GM-example1.png"
-            title="Game mechanic example1"
-          />
-          <GameCard
-            imageSrc="/icons/GM-example2.png"
-            title="Game mechanic example2"
-=======
             imageSrc="/images-game-desc/Dark.png"
             title="Goth Castle"
           />
           <GameCard
             imageSrc="/images-game-desc/Grey.png"
             title="Toxic Zone"
->>>>>>> t7/feature/footer-layout
           />
         </div>
 
         <div className="w-full mt-6 mb-6">
           <GameCard
-<<<<<<< HEAD
-            imageSrc="/icons/GM-example3.png"
-            title="Game mechanic example3"
-=======
             imageSrc="/images-game-desc/Atomic.png"
             title="Bio-Punk Horror"
->>>>>>> t7/feature/footer-layout
           />
         </div>
       </main>
@@ -54,4 +37,3 @@ export default function Home() {
   
   );
 }
-
