0de4ffa7caf176be55d73d0797e72e0db39c8766
SSSpac <saifeisa820@gmail.com>
2025-06-23 17:05:55 +0200

    added more information about the other teams, photos, desc,

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -4,26 +4,22 @@ export const GameDesignTeam: TeamMember[] = [
   {
     name: "Zonghao Wu",
     role: "PO, Lvl - Gameplay",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-designers/Zhong.png",
+    desc: " Product Owner, Gameplay & Combat System Design, Level Design Favorite Games: Persona 5, Mass Effect Trilogy, Dark Souls Trilogy",
     team: "Game Designers",
   },
   {
     name: "Jacob Yosef",
     role: "System Generalist",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-designers/Jacob.png",
+    desc: " Technical and Boss design, Favorite game: Zelda Ocarina of Time",
     team: "Game Designers",
   },
   {
     name: "Max Hjort",
     role: "Audio Designer",
-    photo: "/team-members/game-designers/designers-max.png",
-    desc: "Aspiring technical designer with an extensive background in music and audio production. Combining their previous audio experience with their technical knowledge, Max designed and implemented audio using Reaper, FL Studio and Wwise.",
-    favorites:
-      "Resident Evil 2 Remake, Metal Gear Solid 2, Dark Souls, Amnesia The Bunker, Death Stranding, Deus Ex",
+    photo: "/team-members/game-designers/max.png",
+    desc: "Aspiring technical designer with an extensive background in music and audio production. Combining their previous audio experience with their technical knowledge, Max designed and implemented audio using Reaper, FL Studio and Wwise. Favorite games: Resident Evil 2 Remake, Metal Gear Solid 2, Dark Souls, Amnesia The Bunker, Death Stranding, Deus Ex",
     team: "Game Designers",
   },
 ];
@@ -32,26 +28,22 @@ export const GameArtistTeam: TeamMember[] = [
   {
     name: "Max Strömdal Erikssojn",
     role: "Character Artist",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-artists/max.png",
+    desc: " Character Artist, Art director Favorite game Elden Ring ",
     team: "Game Artists",
   },
   {
     name: "Xiyuan Wang",
     role: "Environment - Artist",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-artists/Xiyuan.png",
+    desc: "Environment Artist, focused on environment art and hard surface props. Skilled in Maya, Substance, ZBrush, and Unreal Engine, with a strong PBR and real-time workflow. Favourite games: Zelda Breath of the Wild, The Last of Us 1 & 2, Assassin's Creed Odyssey",
     team: "Game Artists",
   },
   {
     name: "Ira Aspelin",
     role: "Character prop artist",
     photo: "/team-members/game-artists/artists-ira.png",
-    desc: "Experience with 2D art with a passion for characters and their stories. Worked on Fireflight in GP1, Bungeon in GP2.",
-    favorites:
-      "Lies of P, Final Fantasy XIV, World of Warcraft, Okami, Baldur's Gate 3.",
+    desc: "Experience with 2D art with a passion for characters and their stories. Worked on Fireflight in GP1, Bungeon in GP2. Favourite games: Lies of P, Final Fantasy XIV, World of Warcraft, Okami, Baldur's Gate 3.",
     team: "Game Artists",
   },
 ];
@@ -60,33 +52,30 @@ export const GameProgrammerTeam: TeamMember[] = [
   {
     name: "Kiran Gaurdian George",
     role: "Game Programmer",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-programmers/Kiran.png",
+    desc: "I’m from India with a background in Computer Science. Worked on gunplay and weapon system , Favourite games: Monster Hunter World, Yakuza series, Devil May Cry",
     team: "Game Programmers",
   },
   {
-    name: "Mohammed Diab",
+    name: "Rolf Mohammed Diab",
     role: "Game Programmer",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-programmers/Mohammed.png",
+    desc: " AI, audio programming and system programming, Favorite games: Nier Automata and Persona 5",
+
     team: "Game Programmers",
   },
   {
     name: "Tiger Martin",
     role: "Game Programmer",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-programmers/Tiger.png",
+    desc: "I was the lead programmer of the team, organizing the structure of our systems as well as implementing the spatial inventory and such. Favourite games: Minecraft, Rainbow Six: Siege, GTFO ",
     team: "Game Programmers",
   },
   {
     name: "Sebastian Valck",
     role: "Game Programmer",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/game-programmers/sebastian.png",
+    desc: " Backend programmer, Favorite game: Mega Man 5",
     team: "Game Programmers",
   },
 ];
@@ -95,17 +84,15 @@ export const AnimVfxTeam: TeamMember[] = [
   {
     name: "Jesper Turesson",
     role: "VFX Artist",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/animvfx/jesper.png",
+    desc: "As the VFX artist on the team, I created real-time visual effects using Unreal Engine’s Niagara system, designing particle systems and custom materials for both environmental and gameplay elements. Destiny 2, Path of Exile, Leauge of Legends, Elden Ring, Space Marines 2 and The Finals.",
     team: "ANIM & VFX",
   },
   {
     name: "Sushil Jangra",
     role: "Animator",
-    photo: "",
-    desc: "",
-    favorites: "",
+    photo: "/team-members/animvfx/Sushil.png",
+    desc: "Hi, I’m Sushil Jangra, and I was the sole animator on our 16-person team. I took care of all the character rigging and animation for the game. My focus was on creating animations that felt natural, expressive, creepy, and matched the style of the game we built. It was a great opportunity to apply both the technical and creative sides of animation, and to contribute to the overall feel and playability of the game. Favourite games: Assassin’s creed",
     team: "ANIM & VFX",
   },
 ];
@@ -116,7 +103,6 @@ export const ManagementTeam: TeamMember[] = [
     role: "Game Producer",
     photo: "",
     desc: "",
-    favorites: "",
     team: "Management",
   },
   {
@@ -124,7 +110,6 @@ export const ManagementTeam: TeamMember[] = [
     role: "Project Manager",
     photo: "",
     desc: "",
-    favorites: "",
     team: "Management",
   },
   {
@@ -132,7 +117,6 @@ export const ManagementTeam: TeamMember[] = [
     role: "Release Manager",
     photo: "",
     desc: "",
-    favorites: "",
     team: "Management",
   },
   {
@@ -140,7 +124,6 @@ export const ManagementTeam: TeamMember[] = [
     role: "Release Manager",
     photo: "",
     desc: "",
-    favorites: "",
     team: "Management",
   },
 ];
@@ -151,7 +134,6 @@ export const WebDevelopmentTeam: TeamMember[] = [
     role: "Project Manager",
     photo: "/team-members/web/web-camilla.png",
     desc: "With experience across e-commerce, UX, and customer experience strategy at fast-paced companies, I've worked hands-on with everything from journey mapping and UX & gamification design to stakeholder workshops and agile methodologies. I thrive in environments where innovation meets structure, and where the user perspective drives decision-making.",
-    favorites: "",
     team: "Web Developers",
   },
   {
@@ -159,7 +141,6 @@ export const WebDevelopmentTeam: TeamMember[] = [
     role: "Web Developer",
     photo: "/team-members/web/web-srilatha.png",
     desc: "A passionate learner focused on creating interactive, responsive websites while developing skills as a frontend developer.",
-    favorites: "",
     team: "Web Developers",
   },
   {
@@ -167,7 +148,6 @@ export const WebDevelopmentTeam: TeamMember[] = [
     role: "Web Developer",
     photo: "/team-members/web/web-dimitar.png",
     desc: "A Bulgarian who lost his way to Madrid and ended up in Stockholm",
-    favorites: "",
     team: "Web Developers",
   },
   {
@@ -199,7 +179,6 @@ export const WebDevelopmentTeam: TeamMember[] = [
     role: "Web Developer",
     photo: "/team-members/web/web-bushra.png",
     desc: "Driven by curiosity and continuous learning, I'm always exploring new tools and best practices to deliver efficient and high-performance web applications.",
-    favorites: "",
     team: "Web Developers",
   },
 ];
