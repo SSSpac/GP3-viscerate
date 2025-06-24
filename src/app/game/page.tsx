74ce91de1e3df01e01d1ca9b6b898b839853770a
Lvhacker1 <lavasha2@hotmail.com>
2025-06-25 00:31:13 +0200

    fixes

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,11 +1,10 @@
 import ScrollDownButton from "../../components/ScrollDownButton";
 import FrameWrapper from "../../components/FrameWrapper";
 import DescriptionSection from "../../components/DescriptionSection";
-import { GameHeroProps } from "@/types/globals";
 import { gameHeroText } from "@/data/data";
 import GameHeroTextBlock from "@/components/GameHeroTextBlock";
 
-const GamePage = ({pageTitle = gameHeroText.pageTitle, paragraph = gameHeroText.paragraph }: GameHeroProps) => {
+const GamePage = () => {
   return (
     <>
          <section className="mb-30 w-[100%] h-full relative font-inter">
@@ -19,7 +18,7 @@ const GamePage = ({pageTitle = gameHeroText.pageTitle, paragraph = gameHeroText.
           </FrameWrapper>
         </div>
         <div>
-          <GameHeroTextBlock pageTitle={pageTitle} paragraph={paragraph} />
+          <GameHeroTextBlock pageTitle={gameHeroText.pageTitle} paragraph={gameHeroText.paragraph} />
         </div>
         <div className="absolute -bottom-30 md:-bottom-18 left-1/2 transform -translate-x-1/2 z-10">
           <ScrollDownButton text="Scroll for more info" navigateTo="content" />
