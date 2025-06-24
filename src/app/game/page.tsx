5ebc2d194fa5a1a9f7bc05e06321ec1bf30bf2b2
Lvhacker1 <lavasha2@hotmail.com>
2025-06-24 23:00:16 +0200

    fix image, add text, and some styling

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,30 +1,30 @@
 import ScrollDownButton from "../../components/ScrollDownButton";
 import FrameWrapper from "../../components/FrameWrapper";
 import DescriptionSection from "../../components/DescriptionSection";
-import Image from "next/image";
+import { GameHeroProps } from "@/types/globals";
+import { gameHeroText } from "@/data/data";
 
-const GamePage = () => {
+const GamePage = ({pageTitle = gameHeroText.pageTitle, paragraph = gameHeroText.paragraph }: GameHeroProps) => {
   return (
     <>
          <section className="mb-30 w-[100%] h-full relative font-inter">
         <div className="">
           <FrameWrapper>
             <img
-              src="/images/bg_9.png"
+              src="/images/21bg.jpeg"
               alt="mock image"
               className="max-w-full max-h-full object-contain"
             />
           </FrameWrapper>
         </div>
-        <div className="max-w-xl absolute w-full bottom-10 text-center md:top-1/2 md:right-10  lg:right-30 md:bottom-auto md:text-right md:transform md:-translate-y-4/5 md:space-y-8 text-base">
-          <h1 className="md:text-3xl lg:text-5xl p-2">
-          
+        <div className="md:max-w-xl  p-6 absolute w-full bottom-20 text-center md:top-30 md:right-10 lg:max-w-[600px] lg:right-40 lg:top-70 md:text-center md:transform md:-translate-y-4/5 md:space-y-8 text-base flex flex-col justify-self-center lg:text-left">
+          <h1 className="font-aboreto md:text-3xl lg:text-[55px] p-2">
+            {pageTitle}
           </h1>
           <div className="w-full flex items-center">
-          <h4 className="font-['Aboreto'] text-sm md:text-xl lg:text-xl ">
-            Grid Upgrade System – Players can drag and drop upgrade modules into weapon slots, dynamically affecting gameplay.
-            Upgrade Modules – 5–10.
-          </h4>
+          <p className="font-inter text-sm lg:text-xl xl:text-2xl ">
+            {paragraph}
+          </p>
           </div>
         </div>
         <div className="absolute -bottom-30 md:-bottom-18 left-1/2 transform -translate-x-1/2 z-10">
